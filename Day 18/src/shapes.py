e84ef1ffe3b1c940d9c346ceae6c0f8a38fe67c9
Darrell <public+git@radtechdad.com>
2025-03-12 00:20:33 -0700

    cleaned up the code and comments

--- Day 18/src/shapes.py
+++ Day 18/src/shapes.py
@@ -5,7 +5,9 @@ from turtle import Turtle, Screen
 
 def main():
 
+    #####
     # Functions
+    ####################
     def graphics_start():
         screen.title(os.path.basename(__file__))
         screen.setup(500, 500)
@@ -28,49 +30,30 @@ def main():
         b = random.randint(0, 255)
         return r, g, b
 
-    def draw_line(seg_length=100, line_thickness=1, color="black"):
+    def draw_line(seg_length=100, line_thickness=1, color=(0,0,0)):
         monty.width(line_thickness)
         monty.pencolor(color)
         monty.forward(seg_length)
 
-    def draw_shape(seg_length=100, shape_sides=4, line_thickness=1, color="black"):
+    def draw_shape(seg_length=100, shape_sides=4, line_thickness=1, color=(0,0,0)):
         for _ in range(shape_sides):
             monty.right(360 / shape_sides)
             draw_line(seg_length, line_thickness, color)
 
-
+    #####
     # Variables
-    color_set_1 = [
-        "royal blue",
-        "deep sky blue",
-        "turquoise",
-        "forest green",
-        "dark green",
-        "deep pink",
-        "hot pink",
-        "red",
-        "dark orange",
-        "gold"
-    ]
-    color_set_2 = [
-        "purple",
-        "medium purple",
-        "dark violet",
-        "magenta",
-        "tomato",
-        "firebrick",
-        "chocolate",
-        "dark goldenrod",
-        "olive drab",
-        "slate blue"
-    ]
+    ####################
+    color_set_1 = [ "royal blue", "deep sky blue", "turquoise", "forest green", "dark green", "deep pink", "hot pink", "red", "dark orange", "gold" ]
+    color_set_2 = [ "purple", "medium purple", "dark violet", "magenta", "tomato", "firebrick", "chocolate", "dark goldenrod", "olive drab", "slate blue" ]
+
     segment_length = 100
     line_width = 2
     draw_speed = 'fast'
     python_shape = os.path.join("../", "img/", "python.gif")
 
-
+    #####
     # Main
+    ####################
     screen = Screen()
     monty = Turtle()
 
@@ -80,6 +63,11 @@ def main():
         draw_shape(segment_length, sides, line_width, random_rgb_color())
 
     graphics_end()
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
